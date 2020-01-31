f8082f7678e63b63278ed2c731b6e02dead6eac4
Fe-Ordan <seliconpower2@gmail.com>
2020-01-31 08:20:30 +0100

    f

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 //var logger = require('morgan');
 //var cookieParser = require('cookie-parser');
 //var bodyParser = require('body-parser');
-var imgur = require('imgur');
+//var imgur = require('imgur');
 var index = require('./routes/index');
 //var users = require('./routes/users');
 
