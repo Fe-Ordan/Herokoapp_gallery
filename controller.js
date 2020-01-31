5bf4aaba3bd180ba8914d408f3342689d3ee33fd
Fe-Ordan <seliconpower2@gmail.com>
2020-01-31 08:13:47 +0100

    fire

--- controller.js
+++ controller.js
@@ -1,5 +1,51 @@
 
 module.exports.home = (req,res,next)=>{
-	var paths = ['images/a.jpg','images/b.jpg','images/c.jpg'];
-	res.render('gallery', { imgs: paths, layout:false});
+	var paths = ['images/a.jpg','images/b.jpg','images/c.jpg','https://i.imgur.com/jiZqayk.jpg'];
+	const firebase = require('firebase');
+    //  Initialize Firebase
+    //   var config = {
+    //     apiKey: 'xxxxxxx',
+    //     authDomain: 'xxxxxxx',
+    //     databaseURL: 'xxxxxxx',
+    //     projectId: 'xxxxxxx'
+    //   };
+	//   firebase.initializeApp(config);
+	
+	console.log('this is request: '+req)
+	firebase.initializeApp({
+		serviceAccount:"./DiscordProject-dcf90e0b626f.json",
+		databaseURL:"https://discordproject-260511.firebaseio.com/"
+	});
+      var rootRef = firebase
+        .database()
+        .ref()
+        //.child('FilesSent')
+        //.child('-LytqNOFMA1Ghym4bA1E');
+        rootRef.child("Memes").orderByChild('memeLink').limitToFirst(10).once('value',snapshot =>{
+          let list = [];
+        
+            snapshot.forEach(function(snap){
+              valuepost = snap.val();
+              list.push(valuepost.memesLink);
+
+
+            })
+            return list;
+        }).then(val =>{
+          res.render('gallery', { imgs: val, layout:false});
+        })
+
+    //   var urlParams = new URLSearchParams(window.location.search);
+    //   var dbNodeID = urlParams.get('id');
+		
+    //   rootRef.once('value')
+    //     .then(function(snapshot) {
+		//   var dataItem1 = snapshot.val();
+
+		//   console.log('Items : '+dataItem1)
+		// //  document.getElementById('content').innerHTML = dataItem1;
+		//  res.render('gallery', { imgs: paths, layout:false});
+    //     });
+   
+	
 };
